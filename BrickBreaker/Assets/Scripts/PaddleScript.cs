90677ef88e53caf1350687eed4e027c58e23d090
tianle97 <386466921@qq.com>
2019-04-11 13:43:35 +0100

    add scripts for delaygame start, paddle and sceneGUI(tell user if the myo connect)

---
+++ BrickBreaker/Assets/Scripts/PaddleScript.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using LockingPolicy = Thalmic.Myo.LockingPolicy;
+using Pose = Thalmic.Myo.Pose;
+using UnlockType = Thalmic.Myo.UnlockType;
+using VibrationType = Thalmic.Myo.VibrationType;
+using System;
+
+public class PaddleScript : MonoBehaviour {
+
+    private Transform Paddle;
+    private Pose _lastPose = Pose.Unknown;
+    public GameObject myo = null;
+    public float speed;
+    public float rightScreenEdge;
+    public float leftScreenEdge;
+    public GameManager gm;
+    public BallScript bs;
+
+    // Use this for initialization
+    void Start () {
+        Paddle = GameObject.FindGameObjectWithTag("Player").transform;
+        Paddle = gameObject.GetComponent<Transform>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        ThalmicMyo thalmicMyo = myo.GetComponent<ThalmicMyo>();
+        if (gm.gameOver)
+        {
+            return;
+        }
+        
+        
+
+        if (thalmicMyo.pose != _lastPose)
+        {
+            float xm = 0;
+            float x = 0;
+
+            float horizontal = Input.GetAxis("Horizontal");
+            transform.Translate(Vector2.right * horizontal * Time.deltaTime * xm);
+
+            xm -= 150 * Time.deltaTime;
+            x += 150 * Time.deltaTime;
+
+            _lastPose = thalmicMyo.pose;
+            //make the paddlre turn left
+            if (Paddle.position.x > leftScreenEdge && thalmicMyo.pose == Pose.WaveIn)
+            {
+                // Vibrate the Myo armband when a FingersSpread is made.
+                thalmicMyo.Vibrate(VibrationType.Short);
+                Paddle.Translate(new Vector3(xm, 0, 0), Space.Self);
+                ExtendUnlockAndNotifyUserAction(thalmicMyo);
+            }
+            //make the paddle turn right
+            if (transform.position.x < rightScreenEdge && thalmicMyo.pose == Pose.WaveOut)
+            {
+                // Vibrate the Myo armband when a FingersSpread is made.
+                thalmicMyo.Vibrate(VibrationType.Short);
+                Paddle.Translate(new Vector3(x, 0, 0), Space.Self);
+                ExtendUnlockAndNotifyUserAction(thalmicMyo);
+            }
+        }
+    }
+    
+
+    private void OnTriggerEnter2D(Collider2D other)
+     {
+        if (other.CompareTag("extraLife"))
+        {
+            gm.UpdateLives(1);
+            Destroy(other.gameObject);
+        }
+
+        if (other.CompareTag("fasterSpeed"))
+        {
+            bs.speed = bs.speed + 50;
+            Destroy(other.gameObject);
+        }
+
+        if (other.CompareTag("lowerSpeed"))
+        {
+            bs.speed = bs.speed - 50;
+            Destroy(other.gameObject);
+        }
+
+        if (other.CompareTag("widePaddle"))
+        {
+            GetComponent<Transform>().localScale = new Vector2(20, 2f);
+            Destroy(other.gameObject);
+        }
+
+        if (other.CompareTag("thinPaddle"))
+        {
+            GetComponent<Transform>().localScale = new Vector2(10, 2f);
+            Destroy(other.gameObject);
+        }
+
+
+    }
+
+   
+
+    void ExtendUnlockAndNotifyUserAction(ThalmicMyo myo)
+    {
+        ThalmicHub hub = ThalmicHub.instance;
+
+        if (hub.lockingPolicy == LockingPolicy.Standard)
+        {
+            myo.Unlock(UnlockType.Timed);
+        }
+
+        myo.NotifyUserAction();
+    }
+}
---
+++ BrickBreaker/Assets/Scripts/SceneGUI.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Draw simple instructions for sample scene.
+// Check to see if a Myo armband is paired.
+public class SceneGUI : MonoBehaviour
+{
+    // Myo game object to connect with.
+    // This object must have a ThalmicMyo script attached.
+    public GameObject myo = null;
+
+    // Draw some basic instructions.
+    void OnGUI()
+    {
+        GUI.skin.label.fontSize = 20;
+
+        ThalmicHub hub = ThalmicHub.instance;
+
+        // Access the ThalmicMyo script attached to the Myo object.
+        ThalmicMyo thalmicMyo = myo.GetComponent<ThalmicMyo>();
+
+        if (!hub.hubInitialized)
+        {
+            GUI.Label(new Rect(12, 8, Screen.width, Screen.height),
+                "\n\n\nCannot contact Myo Connect. Is Myo Connect running?\n" +
+                "Press Q to try again."
+            );
+        }
+        else if (!thalmicMyo.isPaired)
+        {
+            GUI.Label(new Rect(12, 8, Screen.width, Screen.height),
+                "\n\n\nNo Myo currently paired."
+            );
+        }
+        else if (!thalmicMyo.armSynced)
+        {
+            GUI.Label(new Rect(12, 8, Screen.width, Screen.height),
+                "\n\n\nPerform the Sync Gesture."
+            );
+        }
+        else
+        {
+            GUI.Label(new Rect(12, 8, Screen.width, Screen.height),
+                "\n\nFist: release ball.\n" +
+                "Wave in: turn left\n" +
+                "Wave out: turn right\n"
+            );
+        }
+    }
+
+    void Update()
+    {
+        ThalmicHub hub = ThalmicHub.instance;
+
+        if (Input.GetKeyDown("q"))
+        {
+            hub.ResetHub();
+        }
+    }
+}
